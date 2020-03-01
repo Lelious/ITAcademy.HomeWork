39252ea8efd9a57f84f34a653e97561794fb1fc3
Roman Pupko <60660149+Lelious@users.noreply.github.com>
2020-03-01 15:59:22 +0300

    Added a programm code

--- Solution1/HW.02/Program.cs
+++ Solution1/HW.02/Program.cs
@@ -1,20 +1,26 @@
-﻿namespace HW._02
+﻿using System;
+using System.IO;
+
+namespace HW._02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /* 
-             * 1. Читаем текстовый файл из определённого пути.
-             * 2. Записываем прочитанные данные в переменную (стринговую).
-             * 3. Убираем пробелы между строками.
-             * 4. Создаём массив байтов (с длинной массива Length-1 т.к. исчисление начинается с 0-го элемента массива).
-             * 5. Создаём цикл, длинной равной Length - 1 и с итератором "i", который инкрементируем после каждого прохода цикла.
-             * 6. В цикле конвертируем каждый элемент массива из строки в байты и записываем в переменную "binary".
-             * 7. Присваиваем переменной "imageBytes[i]" значение переменной "binary".
-             * 8. Записываем все байты из массива "imageBytes" в файл "image.png" по заданному пути.
-             * 9. Очищаем переменную "textReader" методом "Dispose".           
-             */
+            StreamReader textReader = new StreamReader(@"C:\Temp\image.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+
+            File.WriteAllBytes(@"C:\Temp\image.png", imageBytes);
+
+            textReader.Dispose();
         }
     }
 }
