d7f9878645d5e72544b65a716e7a8ceeeaed98e2
Roman Pupko <60660149+Lelious@users.noreply.github.com>
2020-03-30 01:11:43 +0300

    Completed HomeWork_08 Tasks

---
+++ HomeWork_08/HomeWork_08/HomeWork_08/Task_01.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Task_01
+{
+    class Task_01
+    {
+        static bool fixAll;
+        static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Введите стихотворение в одну строку, разделяя его знаком ;");
+
+            var enteredString = Console.ReadLine();
+            sb.Append(enteredString);
+
+            Console.WriteLine("Если вы хотите заменить все буквы О на букву А - введите 1, если только заглавные - 0");
+
+            fixAll = Convert.ToBoolean(Convert.ToByte(Console.ReadLine()));
+            sb.Replace(';', '\n');
+            Replace_A_to_O_Method();
+        }
+        static void Replace_A_to_O_Method()
+        {
+            if (fixAll == false)
+            {
+                sb.Replace('О', 'А');
+                Console.WriteLine(sb);
+            }
+            else
+            {
+                sb.Replace('O', 'A');
+                sb.Replace('o', 'a');
+                Console.WriteLine(sb);
+            }
+        }
+    }
+}
