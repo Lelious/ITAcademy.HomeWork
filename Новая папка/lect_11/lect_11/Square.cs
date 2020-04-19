c5143c47409bdeedb658abbb3e0939685dbfbd89
Roman Pupko <60660149+Lelious@users.noreply.github.com>
2020-04-19 13:45:49 +0300

    Completed HomeWork 11 task

---
+++ Новая папка/lect_11/lect_11/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lect_11
+{
+    class Square : Figure
+    {
+        public Square(int value)
+        {
+            side = value;
+        }
+        public override void CalculateArea()
+        {
+            area = side * side;
+            Console.WriteLine($"Area of Square = {area}");
+        }
+    }
+}
