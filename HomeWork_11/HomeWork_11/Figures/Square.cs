3170e881979191d54d78cc7188c48e7e3bbf7b16
Roman Pupko <60660149+Lelious@users.noreply.github.com>
2020-05-19 10:01:54 +0300

    Add missed homework_11

---
+++ HomeWork_11/HomeWork_11/Figures/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeWork_11
+{
+    class Square : Figure
+    {
+        public Square(int value)
+        {
+            Name = "Square";
+            Side = value;
+        }
+        public override double CalculateArea()
+        {
+            Area = Side * Side;
+            return Area;
+        }
+    }
+}
